96d0f8009fc3122b9e217981f98d12aa8c3102bb
gabriel <gabriel.pinheiro7780@gmail.com>
2024-03-14 12:08:50 -0300

    função editar usuario

--- src/components/users/SearchUser.vue
+++ src/components/users/SearchUser.vue
@@ -95,6 +95,103 @@
                     </button>
                     </td>
 
+                    <td>
+                      <button
+                      @click.prevent="editarUsuario($event)"
+                      class="btn btn-secondary"
+                      ref="editButton"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal">Editar
+                      </button>
+
+                      <div
+                      class="modal fade"
+                      id="exampleModal"
+                      tabindex="-1"
+                      aria-labelledby="exampleModalLabel"
+                      aria-hidden="true">
+
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+
+                              <h1
+                              class="modal-title fs-5"
+                              id="exampleModalLabel">Editar Usuário
+                              </h1>
+
+                              <button
+                              type="button"
+                              class="btn-close"
+                              data-bs-dismiss="modal"
+                              aria-label="Close">
+                              </button>
+
+                            </div>
+                            <div class="modal-body">
+
+                              <form action="">
+                                <div class="row mb-3">
+                                  <div class="col">
+                                    <label for="novoNome">Nome Completo</label>
+                                    <input v-model.trim="novoNome" type="text" id="novoNome" class="form-control">
+                                  </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                  <div class="col">
+                                    <label for="novoUser">Usuário</label>
+                                    <input v-model.trim="novoUser" type="text" id="novoUser" class="form-control">
+                                  </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                  <div class="col">
+                                    <label for="novoCPF">CPF</label>
+                                    <input v-model.trim="novoCPF" type="text" id="novoCPF" class="form-control">
+                                  </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                  <div class="col">
+                                    <label for="novoEndereco">Endereço</label>
+                                    <input v-model.trim="novoEndereco" type="text" id="novoEndereco" class="form-control">
+                                  </div>
+                                </div>
+
+                                <div v-if="editarUsuarioVazio" class="row">
+                                  <div class="col">
+                                    <div class="alert alert-danger" role="alert">
+                                      Preencha todos os campos !!!
+                                    </div>
+
+                                  </div>
+                                </div>
+
+                              </form>
+
+                            </div>
+                            <div class="modal-footer">
+
+                              <button
+                              type="button"
+                              class="btn btn-secondary"
+                              data-bs-dismiss="modal">Cancelar
+                              </button>
+
+                              <button
+                              @click="salvarUsuarioEditado()"
+                              type="button"
+                              class="btn btn-primary">Salvar Alterações
+                              </button>
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                    </td>
+
                 </tr>
             </tbody>
         </table>
@@ -116,6 +213,16 @@ export default {
     const ehVazio = ref(false)
     const renderizar = ref(false)
     const usuarioBuscadoResult = ref({})
+    const editButton = ref(null)
+
+    const novoNome = ref('')
+    const novoUser = ref('')
+    const novoCPF = ref('')
+    const novoEndereco = ref('')
+
+    const usuarioAntigo = ref({})
+
+    const editarUsuarioVazio = ref(false)
 
     function buscarUsuario () {
       const nomeBuscado = nomeBuscadoInput.value.value
@@ -153,14 +260,68 @@ export default {
       window.location.reload()
     }
 
+    function editarUsuario(event) {
+      const linha = event.target.parentElement.parentElement.childNodes
+
+      const nomeAntigo = linha[0].innerText
+      const userAntigo = linha[1].innerText
+      const cpfAntigo = linha[2].innerText
+      const enderecoAntigo = linha[3].innerText
+
+      novoNome.value = nomeAntigo
+      novoCPF.value = cpfAntigo
+      novoUser.value = userAntigo
+      novoEndereco.value = enderecoAntigo
+
+      usuarioAntigo.value = {
+        usuario: userAntigo,
+        nome: nomeAntigo,
+        endereco: enderecoAntigo,
+        cpf: cpfAntigo
+      }
+    }
+
+    function salvarUsuarioEditado () {
+      if (novoNome.value === '' || novoUser.value === '' || novoCPF.value === '' || novoEndereco === '') {
+        editarUsuarioVazio.value = true
+
+        setTimeout(() => {editarUsuarioVazio.value = false}, 4000)
+
+      } else {
+        const usuarioSelecionado = usuarios.filter((usuario) => {
+          return usuario.cpf === usuarioAntigo.value.cpf
+        })
+
+        // console.log(usuarioAntigo.value)
+        // console.log(usuarioSelecionado)
+        usuarioSelecionado[0].nome = novoNome.value
+        usuarioSelecionado[0].cpf = novoCPF.value
+        usuarioSelecionado[0].usuario = novoUser.value
+        usuarioSelecionado[0].endereco = novoEndereco.value
+        
+        localStorage.setItem('usuarios', JSON.stringify(usuarios))
+
+        window.location.reload()
+      }
+    }
+
     return {
       usuarios,
       nomeBuscadoInput,
       ehVazio,
       renderizar,
+      editButton,
+      novoNome,
+      novoUser,
+      novoCPF,
+      novoEndereco,
       usuarioBuscadoResult,
+      editarUsuarioVazio,
+      usuarioAntigo,
       buscarUsuario,
-      deletarLinha
+      deletarLinha,
+      editarUsuario,
+      salvarUsuarioEditado
     }
   }
 }
